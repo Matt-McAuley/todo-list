9a6390480506ec61ae33de997c88e5440501ca78
Matthew McAuley <mattwilliam618@gmail.com>
2024-01-02 20:36:23 -0500

    Created project class to hold todos
    Needed projects to be a "folder" that contains todos

--- src/js-files/todo.js
+++ src/js-files/todo.js
@@ -1,4 +1,4 @@
-export default class Todo {
+class Todo {
 
     constructor(title, description, dueDate, priority) {
         this.title = title;
@@ -7,5 +7,24 @@ export default class Todo {
         this.priority = priority;
         this.completed = false;
     }
+}
 
+class Project {
+
+    constructor(title) {
+        this.todos = [];
+        this.title = title;
+    }
+
+    addTodo(todo) {
+        this.todos.push(todo);
+        this.todos.sort(function(a,b) {
+            return a.dueDate - b.dueDate;
+        });
+    }
+
+    removeTodo(todo) {
+        let i = this.todos.indexOf(todo);
+        this.todos.splice(i, 1);
+    }
 }
