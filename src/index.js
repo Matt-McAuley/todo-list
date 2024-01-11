1761fe6b9b727a64391c4d5cea92d3b843aab308
Matthew McAuley <mattwilliam618@gmail.com>
2024-01-10 20:07:00 -0500

    Added todo creation and display in main section
    Created event listeners to allow for actual adding of todos

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
 import { Todo, Project } from "./js-files/classes";
-import pageLoad from "./js-files/pageLoad";
+import {pageLoad, removePopup} from "./js-files/pageLoad";
 import './style.css';
 
 pageLoad();
 
 let general = new Project('General');
 
-function displayTodos(project) {
-    todoArea = document.querySelector(".todoArea");
-    for (todo in project.todos) {
+const todoArea = document.querySelector(".todoArea");
+
+const todoPopup = document.querySelector(".todoPopup");
+const projectPopup = document.querySelector(".projectPopup");
+
+const todoTitle = document.querySelector("#todoTitle");
+const todoDescription = document.querySelector("#description");
+const todoDueDate = document.querySelector("#dueDate");
+const todoPriority = document.querySelector("#priority");
+
+const projectTitle = document.querySelector("#projectTitle");
+
+todoPopup.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let title = todoTitle.value;
+    let description = todoDescription.value;
+    let dueDate = todoDueDate.value;
+    let priority = todoPriority.value;
+
+    let todo = new Todo(title, description, dueDate, priority);
+    general.addTodo(todo);
+
+    removePopup();
+
+    addTodoDOM(todo);
+});
+
+function displayAllTodos(project) {
+    for (let i = 0; i < project.todos.length; i++) {
         let todoButton = document.createElement("button");
         todoButton.classList.add("todo");
-        todoButton.textContent = todo.title;
+        todoButton.textContent = project.todos[i].title;
         todoArea.append(todoButton);
     }
+}
+
+function removeAllTodos() {
+    while (todoArea.hasChildNodes()) {
+        todoArea.removeChild();
+    }
+}
+
+function addTodoDOM(todo) {
+    let todoButton = document.createElement("button");
+    todoButton.classList.add("todo");
+    todoButton.textContent = todo.title;
+    todoArea.append(todoButton);
 }
--- src/js-files/pageLoad.js
+++ src/js-files/pageLoad.js
@@ -1,24 +1,14 @@
-import { Project } from "./classes";
+//Gather all buttons/form items
+const generalButton = document.querySelector(".general");
+const addProject = document.querySelector(".addButton");
+const addTodo = document.querySelector(".todoButton");
+const cancelButtons = document.querySelectorAll(".cancel");
+// const projectSubmit = document.querySelector(".projectSubmit");
+const projectDropdown = document.querySelector(".projectSelect");
+const todoPopup = document.querySelector(".todoPopup");
+const projectPopup = document.querySelector(".projectPopup");
 
-export default function pageLoad() {
-    //Gather all buttons/form items
-    const generalButton = document.querySelector(".general");
-    const addProject = document.querySelector(".addButton");
-    const addTodo = document.querySelector(".todoButton");
-    const cancelButtons = document.querySelectorAll(".cancel");
-    const todoSubmit = document.querySelector(".todoSubmit");
-    const projectSubmit = document.querySelector(".projectSubmit");
-    const projectDropdown = document.querySelector(".projectSelect");
-    const todoPopup = document.querySelector(".todoPopup");
-    const projectPopup = document.querySelector(".projectPopup");
-
-    const todoTitle = document.querySelector("#todoTitle");
-    const todoDescription = document.querySelector("#description");
-    const todoDueDate = document.querySelector("#dueDate");
-    const todoPriority = document.querySelector("#priority");
-
-    const projectTitle = document.querySelector("#projectTitle");
-
+function pageLoad() {
     //Add necessary event listeners
     addTodo.addEventListener('click', () => {
         todoPopup.classList.add('active');
@@ -42,25 +32,19 @@ export default function pageLoad() {
 
     for (let i = 0; i < cancelButtons.length; i++) {
         cancelButtons[i].addEventListener('click', () => {
-            todoPopup.classList.remove('active');
-            projectPopup.classList.remove('active');
-    
-            generalButton.disabled = false;
-            addProject.disabled = false;
-            addTodo.disabled = false;
-            projectDropdown.disabled = false;
+            removePopup();
         });
     }
-
-    // todoSubmit.addEventListener('click', () => {
-    //     let title = todoTitle.value;
-    //     let description = todoDescription.value;
-    //     let dueDate = todoDueDate.value;
-    //     let priority = todoPriority.value;
-    // });
 }
 
-function createTodo(title, description, dueDate, priority, project) {
-    todo = new Todo(title, description, dueDate, priority);
-    project.addTodo(todo);
+function removePopup() {
+    todoPopup.classList.remove('active');
+    projectPopup.classList.remove('active');
+
+    generalButton.disabled = false;
+    addProject.disabled = false;
+    addTodo.disabled = false;
+    projectDropdown.disabled = false;
 }
+
+export {pageLoad, removePopup};
